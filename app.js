d02614723c78468fcf5f9aec4ada27d4052b892a
zzhilin <zhilin_li@outlook.com>
2021-02-25 17:51:31 -0800

    needs fix multiple objects

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get('/', logIn.view);
 app.get('/filter', filter.view);
 app.get('/filter2', filter2.view);
 app.get('/filter3', filter3.view);
-app.get('/activity', activity.view);
+//app.get('/activity', activity.view);
 app.get('/setting', setting.view);
 app.get('/about', about.view);
 app.get('/filtered', filtered.view);
@@ -57,6 +57,7 @@ app.get('/project/:id', project.projectInfo);
 app.get('/logIn', logIn.view);
 app.get('/complete', complete.view);
 app.get('/index', index.view);
+app.get('/activity/:id', activity.activityInfo);
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
--- routes/activity.js
+++ routes/activity.js
@@ -2,45 +2,65 @@
  * GET new activity.
  */
 
-exports.view = function(request, response){
-    console.log("activity found");
-    response.render('activity', {
-      'activities': [
-          {   'name': 'Waiting in Line',
-              'image': 'lorempixel.people.1.jpeg',
-              'id': 'project1'
-          },
-          
-          { 	'name': 'Needfinding',
-              'image': 'lorempixel.city.1.jpeg',
-              'id': 'project2'
-          },
-          /*
-          { 	'name': 'Prototyping',
-              'image': 'lorempixel.technics.1.jpeg',
-              'id': 'project3'
-          },
-          { 	'name': 'Heuristic Evaluation',
-              'image': 'lorempixel.abstract.1.jpeg',
-              'id': 'project4'
-          },
-          { 	'name': 'Skeleton and a Plan',
-              'image': 'lorempixel.abstract.8.jpeg',
-              'id': 'project5'
-          },
-          { 	'name': 'Meat on the Bones',
-              'image': 'lorempixel.people.2.jpeg',
-              'id': 'project6'
-          },
-          { 	'name': 'Ready for Testing',
-              'image': 'lorempixel.technics.2.jpeg',
-              'id': 'project7'
-          },
-          { 	'name': 'User Test Results and Online Test Proposal',
-              'image': 'lorempixel.city.2.jpeg',
-              'id': 'project8'
-          } */   	
-      ]
-    });
-  };
-  
+var data = require('../actLog.json');
+
+// exports.view = function(request, response) {
+// 	console.log(data);
+// 	response.render('activity', data);
+// };
+
+exports.activityInfo = function(request, response) {
+	console.log(data);
+	response.render('activity', data);
+	// var projectID = request.params.id;
+	// if (projectID == 'random') {
+	// 	projectID = Math.floor(Math.random() * projects.length) + 1;
+	// } else {
+	// 	projectID = parseInt(projectID);
+	// }
+
+	// var project = projects[projectID - 1]; // of by one, our first project has index 0
+	// response.json(project);
+};
+
+// exports.view = function(request, response){
+//     console.log("activity found");
+//     response.render('activity', {
+//       'activities': [
+//           {   'name': 'Waiting in Line',
+//               'image': 'lorempixel.people.1.jpeg',
+//               'id': 'project1'
+//           },
+
+//           { 	'name': 'Needfinding',
+//               'image': 'lorempixel.city.1.jpeg',
+//               'id': 'project2'
+//           },
+//           /*
+//           { 	'name': 'Prototyping',
+//               'image': 'lorempixel.technics.1.jpeg',
+//               'id': 'project3'
+//           },
+//           { 	'name': 'Heuristic Evaluation',
+//               'image': 'lorempixel.abstract.1.jpeg',
+//               'id': 'project4'
+//           },
+//           { 	'name': 'Skeleton and a Plan',
+//               'image': 'lorempixel.abstract.8.jpeg',
+//               'id': 'project5'
+//           },
+//           { 	'name': 'Meat on the Bones',
+//               'image': 'lorempixel.people.2.jpeg',
+//               'id': 'project6'
+//           },
+//           { 	'name': 'Ready for Testing',
+//               'image': 'lorempixel.technics.2.jpeg',
+//               'id': 'project7'
+//           },
+//           { 	'name': 'User Test Results and Online Test Proposal',
+//               'image': 'lorempixel.city.2.jpeg',
+//               'id': 'project8'
+//           } */
+//       ]
+//     });
+//   };
