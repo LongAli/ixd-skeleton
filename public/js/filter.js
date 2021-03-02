968432f437446076d9f582afa3a497b1d534cd2e
Ali L <alil@alis-mbp-6.lan>
2021-03-02 12:06:10 -0800

    try again

--- public/js/filter.js
+++ public/js/filter.js
@@ -3,10 +3,10 @@ var activities = require('../activites.json');
 var selectTime = window.localStorage.getItem('currentId');
 var selectCategory = window.localStorage.getItem('selectedCategory');
 var selectTag = window.localStorage.getItem('currentSelect');
-console.log(selectTime);
+//console.log(selectTime);
 
 function filterByTime(time) {
 	const results = activities.activites.filter((element) => element.minTime <= time);
-	console.log(results);
+	//console.log(results);
 	return results;
 }
