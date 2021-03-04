bb9db9bdafdbbbcb4dda82d9e2aebd33ede2f357
Ali L <alil@alis-mbp-6.lan>
2021-03-04 14:04:21 -0800

    save feature

--- routes/save.js
+++ routes/save.js
@@ -4,41 +4,57 @@
 
  var data = require('../actLog.json');
  var activities = require('../activites.json');
+ //const fs = require("fs"); 
 
  function search(ele) {
-	var searchCompleted = activities.activites.filter((a) => a.ID <= ele);
+	var searchCompleted = activities.activites.filter((a) => a.ID == ele);
 	return searchCompleted;
 }
 
  exports.saveActivity = function(request, response) {
     console.log("starting to add friends")
-        const activity = request.query.Activity;
-        const time = request.query.minTime;
-        const description = request.query.Description;
-        const image = request.query.Image;
-        const assignedID = request.query.id;
-        var idNumber = Object.keys(data).length;
-    // console.log(idNumber);
-    // console.log("jj");
 
+
+
+    var newActivity;
     if (request.query['activity_index']) {
-		results = { activites: search(request.query['activity_index']) };
-	} else {
-		results = activities;
-    }
+        newActivity = search(request.query['activity_index']);
+        data.toLog.unshift(newActivity[0]);
+	}
 
-    console.log(results);
+    //console.log(newActivity);
+    //var objLength = Object.keys(activities).length;
+   
+    
+    //console.log("jj");
+    //console.log(data);
+    // const assignedID = request.newActivity.ID;
+    // const activity = request.query.Activity;
+    // const time = request.query.minTime;
+    // const description = request.query.Description;
+    // const image = request.query.Image;
 
-    //data[assignedID] = idNumber;
-    const newActivity = {
-        "ID": completedID,
-        "Activity": activity,
-        "minTime": time,
-        "Description": description,
-        "Image": image,
-    };
-    console.log(newActivity)
-    data.toLog.push(newActivity);
+    // console.log(assignedID);
+
+    // //data[assignedID] = idNumber;
+    // const newAc = {
+    //     "ID": assignedID,
+    //     "Activity": activity,
+    //     "minTime": time,
+    //     "Description": description,
+    //     "Image": image,
+    // };
+    //data.toLog.push(newActivity);
+    //var logList = data.toLog.length;
+    //console.log(logList);
+    //data[] = logList + 1;
+    // fs.writeFile("data.json", JSON.stringify(data), err => { 
+     
+    //      // Checking for errors 
+    //      if (err) throw err;  
+       
+    //     console.log("Done writing"); // Success 
+    //  }); 
     response.render('index', data);
  }
 
