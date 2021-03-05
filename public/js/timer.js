18fe0bf91642c41022c6d11b7d3d09787f92cc5c
zzhilin <zhilin_li@outlook.com>
2021-03-04 22:46:56 -0800

    functioning timer

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,10 +1,17 @@
+//var activities = require('../activites.json');
+
+var timeVal = parseInt(document.getElementById('timer').textContent.substr(0, 2));
+//console.log(timeVal);
+
 function timer() {
-	var countdownTime = 10;
+	var countdownTime = timeVal * 60;
 	var time = countdownTime,
 		minutes,
 		seconds;
+	//console.log(time);
 	var x = setInterval(function() {
 		minutes = parseInt(time / 60, 10);
+		console.log('minutes' + minutes);
 		seconds = parseInt(time % 60, 10);
 
 		minutes = minutes < 10 ? '0' + minutes : minutes;
